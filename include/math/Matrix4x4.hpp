976db684bc386adb7ef81678f70d83e957872a59
Zach <duong.nguyen@rochester.edu>
2024-09-25 15:31:38 -0400

    docs(math): :memo: Add documentation for Matrix4x4.

--- include/math/Matrix4x4.hpp
+++ include/math/Matrix4x4.hpp
@@ -27,10 +27,19 @@ namespace t {
 #define n43 elements[14]
 #define n44 elements[15]
 
+/**
+ * A class representing \f$4 \times 4\f$ matrices.
+ */
 class Matrix4x4 {
 public:
-  std::array<double, 16> elements;
+  std::array<double, 16> elements; /**< The array containing the elements of
+                                      this matrix in row-major order. */
 
+  /**
+   * Returns a \f$4 \times 4\f$ identity matrix.
+   *
+   * @returns A \f$4 \times 4\f$ identity matrix.
+   */
   static Matrix4x4 identity() {
     // clang-format off
     return Matrix4x4(1, 0, 0, 0,
@@ -40,6 +49,11 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns a \f$4 \times 4\f$ zero matrix.
+   *
+   * @returns A \f$4 \times 4\f$ zero matrix.
+   */
   static Matrix4x4 zero() {
     // clang-format off
     return Matrix4x4(0, 0, 0, 0,
@@ -49,6 +63,17 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns a translation matrix for the specified translation for
+   * post-multiplication (\f$V' = AV\f$ where \f$V\f$ is the homogeneous
+   * coordinates and \f$A\f$ is the transformation matrix).
+   *
+   * If you want to use pre-multiplication i.e. \f$V' = VA\f$, transpose the
+   * returned matrix.
+   *
+   * @param distance The distance to translate along the x, y, and z axes.
+   * @returns The translation matrix for `distance`.
+   */
   static Matrix4x4 fromTranslation(const Vector3 &distance) {
     // clang-format off
     return Matrix4x4(1, 0, 0, distance.x,
@@ -58,22 +83,29 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns a rotation matrix for the specified rotation for
+   * post-multiplication (\f$V' = AV\f$ where \f$V\f$ is the homogeneous
+   * coordinates and \f$A\f$ is the transformation matrix).
+   *
+   * If you want to use pre-multiplication i.e. \f$V' = VA\f$, transpose the
+   * returned matrix.
+   *
+   * @param angle An Euler rotation.
+   * @returns The rotation matrix for `angle`.
+   */
   static Matrix4x4 fromRotation(const EulerRotation &angle) {
     // clang-format off
     auto rotationX = Matrix4x4(1,                 0,                  0, 0,
                                0, std::cos(angle.x), -std::sin(angle.x), 0,
                                0, std::sin(angle.x),  std::cos(angle.x), 0,
                                0,                 0,                  0, 1);
-    // clang-format on
 
-    // clang-format off
     auto rotationY = Matrix4x4(std::cos(angle.y), 0, std::sin(angle.y), 0,
                                                0, 1,                 0, 0,
                               -std::sin(angle.y), 0, std::cos(angle.y), 0,
                                                0, 0,                 0, 1);
-    // clang-format on
 
-    // clang-format off
     auto rotationZ = Matrix4x4(std::cos(angle.z), -std::sin(angle.z), 0, 0,
                                std::sin(angle.z),  std::cos(angle.z), 0, 0,
                                                0,                  0, 1, 0,
@@ -90,6 +122,12 @@ public:
     return thirdRotation * secondRotation * firstRotation;
   }
 
+  /**
+   * Returns a scale matrix for the specified scale factor.
+   *
+   * @param scaleFactor The scale factor along the x, y, and z axes.
+   * @returns The scale matrix for `scaleFactor`.
+   */
   static Matrix4x4 fromScale(const Vector3 &scaleFactor) {
     // clang-format off
     return Matrix4x4(scaleFactor.x,             0,             0, 0,
@@ -99,6 +137,26 @@ public:
     // clang-format on
   }
 
+  /**
+   * Creates a new \f$4 \times 4\f$ matrix with the specified elements.
+   *
+   * @param _n11 The \f$(1, 1)\f$ element of the matrix.
+   * @param _n12 The \f$(1, 2)\f$ element of the matrix.
+   * @param _n13 The \f$(1, 3)\f$ element of the matrix.
+   * @param _n14 The \f$(1, 4)\f$ element of the matrix.
+   * @param _n21 The \f$(2, 1)\f$ element of the matrix.
+   * @param _n22 The \f$(2, 2)\f$ element of the matrix.
+   * @param _n23 The \f$(2, 3)\f$ element of the matrix.
+   * @param _n24 The \f$(2, 4)\f$ element of the matrix.
+   * @param _n31 The \f$(3, 1)\f$ element of the matrix.
+   * @param _n32 The \f$(3, 2)\f$ element of the matrix.
+   * @param _n33 The \f$(3, 3)\f$ element of the matrix.
+   * @param _n34 The \f$(3, 4)\f$ element of the matrix.
+   * @param _n41 The \f$(4, 1)\f$ element of the matrix.
+   * @param _n42 The \f$(4, 2)\f$ element of the matrix.
+   * @param _n43 The \f$(4, 3)\f$ element of the matrix.
+   * @param _n44 The \f$(4, 4)\f$ element of the matrix.
+   */
   Matrix4x4(double _n11, double _n12, double _n13, double _n14, double _n21,
             double _n22, double _n23, double _n24, double _n31, double _n32,
             double _n33, double _n34, double _n41, double _n42, double _n43,
@@ -106,6 +164,11 @@ public:
       : elements({_n11, _n12, _n13, _n14, _n21, _n22, _n23, _n24, _n31, _n32,
                   _n33, _n34, _n41, _n42, _n43, _n44}) {}
 
+  /**
+   * Returns a copy of this matrix.
+   *
+   * @returns A copy of this matrix.
+   */
   Matrix4x4 clone() const {
     // clang-format off
     return Matrix4x4(n11, n12, n13, n14,
@@ -115,6 +178,12 @@ public:
     // clang-format on
   }
 
+  /**
+   * Copies the elements of another \f$4 \times 4\f$ matrix into this matrix.
+   *
+   * @param source The \f$4 \times 4\f$ matrix to copy from.
+   * @returns This matrix.
+   */
   Matrix4x4 &copy(const Matrix4x4 &source) {
     for (auto i = 0; i < 16; ++i) {
       elements[i] = source.elements[i];
@@ -123,6 +192,27 @@ public:
     return *this;
   }
 
+  /**
+   * Sets the elements of this matrix.
+   *
+   * @param _n11 The new \f$(1, 1)\f$ element of the matrix.
+   * @param _n12 The new \f$(1, 2)\f$ element of the matrix.
+   * @param _n13 The new \f$(1, 3)\f$ element of the matrix.
+   * @param _n14 The new \f$(1, 4)\f$ element of the matrix.
+   * @param _n21 The new \f$(2, 1)\f$ element of the matrix.
+   * @param _n22 The new \f$(2, 2)\f$ element of the matrix.
+   * @param _n23 The new \f$(2, 3)\f$ element of the matrix.
+   * @param _n24 The new \f$(2, 4)\f$ element of the matrix.
+   * @param _n31 The new \f$(3, 1)\f$ element of the matrix.
+   * @param _n32 The new \f$(3, 2)\f$ element of the matrix.
+   * @param _n33 The new \f$(3, 3)\f$ element of the matrix.
+   * @param _n34 The new \f$(3, 4)\f$ element of the matrix.
+   * @param _n41 The new \f$(4, 1)\f$ element of the matrix.
+   * @param _n42 The new \f$(4, 2)\f$ element of the matrix.
+   * @param _n43 The new \f$(4, 3)\f$ element of the matrix.
+   * @param _n44 The new \f$(4, 4)\f$ element of the matrix.
+   * @returns This matrix.
+   */
   Matrix4x4 &set(double _n11, double _n12, double _n13, double _n14,
                  double _n21, double _n22, double _n23, double _n24,
                  double _n31, double _n32, double _n33, double _n34,
@@ -147,15 +237,39 @@ public:
     return *this;
   }
 
+  /**
+   * Gets the element at the specified row and column of this matrix with bounds
+   * checking.
+   *
+   * @param row The 0-based index of the row of the element.
+   * @param column The 0-based index of the column of the element.
+   * @returns The element at the specified row and column of this matrix.
+   *
+   * @see {@link #operator[]} for accessing without bounds checking.
+   */
   double get(int row, int column) const {
     return elements.at(row * 4 + column);
   }
 
+  /**
+   * Sets the element at the specified row and column of this matrix to the
+   * specified value.
+   *
+   * @param row The 0-based index of the row of the element.
+   * @param column The 0-based index of the column of the element.
+   * @param value The new value for the element.
+   * @returns This matrix.
+   */
   Matrix4x4 &set(int row, int column, int value) {
     elements.at(row * 4 + column) = value;
     return *this;
   }
 
+  /**
+   * Resets this matrix to the \f$4 \times 4\f$ identity matrix.
+   *
+   * @returns This matrix.
+   */
   Matrix4x4 &reset() {
     // clang-format off
     return set(1, 0, 0, 0,
@@ -165,6 +279,12 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns the [determinant](https://en.wikipedia.org/wiki/Determinant) of
+   * this matrix.
+   *
+   * @returns The determinant of this matrix.
+   */
   double determinant() const {
     // clang-format off
     return
@@ -177,6 +297,14 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns the [transpose](https://en.wikipedia.org/wiki/Transpose) of this
+   * matrix.
+   *
+   * Note that this method does not modify this instance.
+   *
+   * @returns The transpose of this matrix.
+   */
   Matrix4x4 transpose() const {
     // clang-format off
     return Matrix4x4(n11, n21, n31, n41,
@@ -186,6 +314,18 @@ public:
     // clang-format on
   }
 
+  /**
+   * Returns the inverse of this matrix or the zero matrix if this matrix does
+   * not have an inverse.
+   *
+   * The inverse of a matrix \f$A\f$ is the matrix \f$A^{-1}\f$ such that
+   * \f$AA^{-1} = I\f$ where \f$I\f$ is the identity matrix. A matrix does not
+   * have an inverse if its determinant is zero.
+   *
+   * Note that this method does not modify this instance.
+   *
+   * @returns The inverse of this matrix.
+   */
   Matrix4x4 inverse() const {
     auto det = determinant();
 
@@ -217,12 +357,39 @@ public:
     return (1.0 / det) * adjoint;
   }
 
+  /**
+   * Returns the top-left \f$3 \times 3\f$ matrix of this matrix.
+   *
+   * For a transformation matrix, this method may be used to extract the
+   * rotation and scale components of the transformation, while discarding the
+   * translation component.
+   *
+   * @returns The top-left \f$3 \times 3\f$ matrix of this matrix.
+   */
   Matrix3x3 topLeft3x3Matrix() const {
     return Matrix3x3(n11, n12, n13, n21, n22, n23, n31, n32, n33);
   }
 
+  /**
+   * Returns the element at the specified index of this matrix without bounds
+   * checking and assuming row-major ordering.
+   *
+   * The element at row \f$i\f$ and column \f$j\f$ of this matrix has the index
+   * \f$4i + j\f$ assuming \f$i\f$ and \f$j\f$ are zero-based.
+   *
+   * @param index The 0-based index of the element in row-major order.
+   * @returns The element at the specified index of this matrix.
+   *
+   * @see {@link #get} for accessing with bounds checking.
+   */
   double operator[](std::size_t index) const { return elements.at(index); }
 
+  /**
+   * Adds another \f$4 \times 4\f$ matrix to this matrix.
+   *
+   * @param m A \f$4 \times 4\f$ matrix to add to this matrix.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator+=(const Matrix4x4 &m) {
     for (auto i = 0; i < 16; ++i) {
       elements[i] += m.elements[i];
@@ -231,6 +398,12 @@ public:
     return *this;
   }
 
+  /**
+   * Adds the specified constant to all elements in this matrix.
+   *
+   * @param s A constant to add to the elements of this matrix.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator+=(double s) {
     for (auto &element : elements) {
       element += s;
@@ -239,6 +412,12 @@ public:
     return *this;
   }
 
+  /**
+   * Substracts another \f$4 \times 4\f$ matrix from this matrix.
+   *
+   * @param m A \f$4 \times 4\f$ matrix to substract from this matrix.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator-=(const Matrix4x4 &m) {
     for (auto i = 0; i < 16; ++i) {
       elements[i] -= m.elements[i];
@@ -247,6 +426,12 @@ public:
     return *this;
   }
 
+  /**
+   * Substracts the specified constant from all elements in this matrix.
+   *
+   * @param s A constant to substract from the elements of this matrix.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator-=(double s) {
     for (auto &element : elements) {
       element -= s;
@@ -255,8 +440,24 @@ public:
     return *this;
   }
 
+  /**
+   * Multiplies this matrix with another \f$4 \times 4\f$ matrix and copies the
+   * result into this instance.
+   *
+   * Note that this is not an element-wise multiplication, but [matrix
+   * multiplication](https://en.wikipedia.org/wiki/Matrix_multiplication).
+   *
+   * @param m A \f$4 \times 4\f$ matrix to multiply with.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator*=(const Matrix4x4 &m) { return this->copy(*this * m); }
 
+  /**
+   * Multiplies the elements of this matrix with the specified constant.
+   *
+   * @param s A constant to multiply the elements of this matrix with.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator*=(double s) {
     for (auto &element : elements) {
       element *= s;
@@ -265,6 +466,14 @@ public:
     return *this;
   }
 
+  /**
+   * Divides the elements of this matrix with the elements of another \f$4
+   * \times 4\f$ matrix in an element-wise manner.
+   *
+   * @param m A \f$4 \times 4\f$ matrix to divide the elements of this matrix
+   * with.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator/=(const Matrix4x4 &m) {
     for (auto i = 0; i < 16; ++i) {
       elements[i] /= m.elements[i];
@@ -273,6 +482,12 @@ public:
     return *this;
   }
 
+  /**
+   * Divides the elements of this matrix with the specified constant.
+   *
+   * @param s A constant to divide the elements of this matrix with.
+   * @returns This matrix.
+   */
   Matrix4x4 &operator/=(double s) {
     for (auto &element : elements) {
       element /= s;
@@ -281,8 +496,25 @@ public:
     return *this;
   }
 
+  /**
+   * Returns whether this matrix is equal to another \f$4 \times 4\f$ matrix.
+   *
+   * Two matrices are equal if they have the same size and all their
+   * corresponding elements are equal.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param b Another \f$4 \times 4\f$ matrix.
+   * @returns Whether `a` is equal to `b`.
+   */
   friend bool operator==(const Matrix4x4 &a, const Matrix4x4 &b) = default;
 
+  /**
+   * Adds 2 \f$4 \times 4\f$ matrices.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param b Another \f$4 \times 4\f$ matrix.
+   * @returns The sum of `a` and `b`.
+   */
   friend Matrix4x4 operator+(const Matrix4x4 &a, const Matrix4x4 &b) {
     // clang-format off
     return Matrix4x4(a.n11 + b.n11, a.n12 + b.n12, a.n13 + b.n13, a.n14 + b.n14,
@@ -292,6 +524,13 @@ public:
     // clang-format on
   }
 
+  /**
+   * Adds a constant to all elements of a \f$4 \times 4\f$ matrix.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param s A constant.
+   * @returns The resulting matrix when adding `s` to the elements of `a`.
+   */
   friend Matrix4x4 operator+(const Matrix4x4 &a, double s) {
     // clang-format off
     return Matrix4x4(a.n11 + s, a.n12 + s, a.n13 + s, a.n14 + s,
@@ -301,8 +540,22 @@ public:
     // clang-format on
   }
 
+  /**
+   * Adds a constant to all elements of a \f$4 \times 4\f$ matrix.
+   *
+   * @param s A constant.
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @returns The resulting matrix when adding `s` to the elements of `a`.
+   */
   friend Matrix4x4 operator+(double s, const Matrix4x4 &a) { return a + s; }
 
+  /**
+   * Substracts 2 \f$4 \times 4\f$ matrices.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param b Another \f$4 \times 4\f$ matrix.
+   * @returns The difference of `a` and `b`.
+   */
   friend Matrix4x4 operator-(const Matrix4x4 &a, const Matrix4x4 &b) {
     // clang-format off
     return Matrix4x4(a.n11 - b.n11, a.n12 - b.n12, a.n13 - b.n13, a.n14 - b.n14,
@@ -312,6 +565,14 @@ public:
     // clang-format on
   }
 
+  /**
+   * Substracts a constant from all elements of a \f$4 \times 4\f$ matrix.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param s A constant.
+   * @returns The resulting matrix when substracting `s` from the elements of
+   * `a`.
+   */
   friend Matrix4x4 operator-(const Matrix4x4 &a, double s) {
     // clang-format off
     return Matrix4x4(a.n11 - s, a.n12 - s, a.n13 - s, a.n14 - s,
@@ -321,6 +582,13 @@ public:
     // clang-format on
   }
 
+  /**
+   * Substracts a \f$4 \times 4\f$ matrix from a constant matrix.
+   *
+   * @param s A constant for a \f$4 \times 4\f$ constant matrix.
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @returns The resulting matrix when substracting `a` from `s`.
+   */
   friend Matrix4x4 operator-(double s, const Matrix4x4 &a) {
     // clang-format off
     return Matrix4x4(s - a.n11, s - a.n12, s - a.n13, s - a.n14,
@@ -330,6 +598,16 @@ public:
     // clang-format on
   }
 
+  /**
+   * Multiplies 2 \f$4 \times 4\f$ matrices.
+   *
+   * Note that this is not an element-wise multiplication, but [matrix
+   * multiplication](https://en.wikipedia.org/wiki/Matrix_multiplication).
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param b Another \f$4 \times 4\f$ matrix.
+   * @returns The product of `a` and `b`.
+   */
   friend Matrix4x4 operator*(const Matrix4x4 &a, const Matrix4x4 &b) {
     return Matrix4x4(
         a.n11 * b.n11 + a.n12 * b.n21 + a.n13 * b.n31 + a.n14 * b.n41,
@@ -350,6 +628,14 @@ public:
         a.n41 * b.n14 + a.n42 * b.n24 + a.n43 * b.n34 + a.n44 * b.n44);
   }
 
+  /**
+   * Multiplies all elements of a \f$4 \times 4\f$ matrix with a constant.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param s A constant.
+   * @returns The resulting matrix when multiplying the elements of `a` with
+   * `s`.
+   */
   friend Matrix4x4 operator*(const Matrix4x4 &a, double s) {
     // clang-format off
     return Matrix4x4(a.n11 * s, a.n12 * s, a.n13 * s, a.n14 * s,
@@ -359,8 +645,23 @@ public:
     // clang-format on
   }
 
+  /**
+   * Multiplies all elements of a \f$4 \times 4\f$ matrix with a constant.
+   *
+   * @param s A constant.
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @returns The resulting matrix when multiplying the elements of `a` with
+   * `s`.
+   */
   friend Matrix4x4 operator*(double s, const Matrix4x4 &a) { return a * s; }
 
+  /**
+   * Multiplies a \f$4 \times 4\f$ matrix with a four-dimensional vector.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param v A four-dimensional vector.
+   * @returns The product of `a` and `v`.
+   */
   friend Vector4 operator*(const Matrix4x4 &a, const Vector4 &v) {
     return Vector4(a.n11 * v.x + a.n12 * v.y + a.n13 * v.z + a.n14 * v.w,
                    a.n21 * v.x + a.n22 * v.y + a.n23 * v.z + a.n24 * v.w,
@@ -368,6 +669,15 @@ public:
                    a.n41 * v.x + a.n42 * v.y + a.n43 * v.z + a.n44 * v.w);
   }
 
+  /**
+   * Divides the elements of a \f$4 \times 4\f$ matrix by the elements of
+   * another \f$4 \times 4\f$ matrix in an element-wise manner.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param b Another \f$4 \times 4\f$ matrix.
+   * @returns The resulting matrix when dividing the elements of `a` by the
+   * corresponding elements of `b`.
+   */
   friend Matrix4x4 operator/(const Matrix4x4 &a, const Matrix4x4 &b) {
     // clang-format off
     return Matrix4x4(a.n11 / b.n11, a.n12 / b.n12, a.n13 / b.n13, a.n14 / b.n14,
@@ -377,6 +687,13 @@ public:
     // clang-format on
   }
 
+  /**
+   * Divides the elements of a \f$4 \times 4\f$ matrix by a constant.
+   *
+   * @param a A \f$4 \times 4\f$ matrix.
+   * @param s A constant.
+   * @returns The resulting matrix when dividing the elements of `a` by `s`.
+   */
   friend Matrix4x4 operator/(const Matrix4x4 &a, double s) {
     // clang-format off
     return Matrix4x4(a.n11 / s, a.n12 / s, a.n13 / s, a.n14 / s,
@@ -386,6 +703,15 @@ public:
     // clang-format on
   }
 
+  /**
+   * Divides the elements of a \f$4 \times 4\f$ constant matrix by the elements
+   * of another \f$4 \times 4\f$ matrix in an element-wise manner.
+   *
+   * @param s A constant for a \f$3 \times 4\f$ constant matrix.
+   * @param a Another \f$4 \times 4\f$ matrix.
+   * @returns The resulting matrix when dividing the elements of `s` by the
+   * corresponding elements of `a`.
+   */
   friend Matrix4x4 operator/(double s, const Matrix4x4 &a) {
     // clang-format off
     return Matrix4x4(s / a.n11, s / a.n12, s / a.n13, s / a.n14,
